9d4240e13e3275ba7c53cf2e63a4d8fdde4aa843
000626574705 <shreetikagmm123@gmail.com>
2024-04-18 21:48:17 +0530

    upto Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link} from "react-router-dom";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <Link to="/">
+      Intuitive Quiz Hub
+    </Link>
+  )
+}
+
+export default Header
